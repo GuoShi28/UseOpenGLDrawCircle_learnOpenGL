762c1bc9b0f1c9ecb9c2be7008bf3b7f5b7fccf8
GuoShi28 <1747813831@qq.com>
2016-05-24 21:00:04 +0800

    Add files via upload

--- drawSphereUsingTexture/main.cpp
+++ drawSphereUsingTexture/main.cpp
@@ -78,7 +78,59 @@ void init(void)
 	roangles = 45.0f;
 }
 
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(roangles, 0.0, 0.0, 1.0);
+#ifdef GL_VERSION_1_1
+	glBindTexture(GL_TEXTURE_1D, texName);
+#endif
+	//glutSolidTeapot(2.0);
+	glutSolidSphere(2.0, 32, 32);
+	glPopMatrix();
+	glFlush();
+}
 
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (w <= h)
+	{
+		glOrtho(-3.5, 3.5, -3.5*(GLfloat)h / (GLfloat)w,
+			3.5*(GLfloat)h / (GLfloat)w, -3.5, 3.5);
+	}
+	else
+	{
+		glOrtho(-3.5*(GLfloat)w / (GLfloat)h, 3.5*(GLfloat)w / (GLfloat)h,
+			-3.5, 3.5, -3.5, 3.5);
+	}
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void idle()
+{
+	roangles += 0.1f;
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(256, 256);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	glutIdleFunc(idle);	//use?
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+	return 0;
+}
 
 
 
